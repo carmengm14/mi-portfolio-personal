9587a813cab8ad431d90f2d67d60e6bcfd099416
carmengm14 <carmen2023@borealos.com>
2026-05-05 17:03:30 +0200

    seccion sobre mi con animacion y boton

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,88 @@
   <div id="app">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
+      rel="stylesheet">
+    <link rel="stylesheet" href="../src/style.css">
     <NavBar />
     <main>
-      <router-view/>
+      <router-view />
     </main>
     <FooterBar />
   </div>
 </template>
 
 <script>
-import NavBar from './components/NavBar.vue'
+import NavBar from "./components/NavBar.vue";
 import FooterBar from "./components/FooterBar.vue";
 
 export default {
   name: "App",
   components: {
     NavBar,
-    FooterBar
-  }
+    FooterBar,
+  },
+
+  mounted() {
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+            this.observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.2 }
+    );
+
+    this.observeElements();
+  },
+
+  beforeUnmount() {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  },
+
+  methods: {
+    observeElements() {
+      const elements = document.querySelectorAll(".fade-element");
+
+      elements.forEach((el) => {
+        if (!el.classList.contains("visible")) {
+          this.observer.observe(el);
+        }
+      });
+    },
+  },
 };
 </script>
 
 <style>
+/*ANIMACIONES DE ELEMENTOS*/
+/* estado inicial */
+.fade-element {
+  opacity: 0;
+  transform: translateY(20px);
+  transition: all 0.8s ease;
+}
 
+/* cuando aparece */
+.fade-element.visible {
+  opacity: 1;
+  transform: translateY(0);
+}
 
+@keyframes fadeUp {
+  from {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,19 +9,83 @@
         <h2>GARCÍA</h2>
       </div>
     </div>
+    <div class="body">
+      <div class="sobre-mi">
+        <div class="header">
+          <div>
+            <p class="section-title  fade-element">Sobre mí</p>
+            <h1 class="fade-element" style="margin-top: 0">Hola!</h1>
+          </div>
+
+          <!-- SVG derecha -->
+          <div class="firma  fade-element">
+            <img src="../assets/cgm.svg" alt="">
+          </div>
+        </div>
+
+        <div class="explicacionTextos  fade-element">
+          <div class="izquierda  fade-element">
+            <p>Soy Carmen García, una apasionada del mundo digital nacida y criada en Valencia. Desde siempre me ha
+              llamado la atención la tecnología, lo que me llevó a estudiar dos grados en informática, el último
+              especializado en desarrollo de aplicaciones web. Pero si algo cambió mi rumbo, fue la asignatura de
+              diseño: ahí descubrí mi verdadera vocación.</p>
+            <button class="btn-amarillo" @click="$router.push('/sobre-mi')">Conóceme más   <span class="arrow">→</span></button>
+          </div>
+
+          <div class="derecha  fade-element">
+            <p>Desde entonces, me he sumergido de lleno en el universo del diseño web, explorando UX/UI, accesibilidad y
+              SEO, elementos clave para crear experiencias digitales que no solo sean visualmente atractivas, sino
+              también funcionales e inclusivas. Esto me ha llevado a aprender y perfeccionar herramientas como
+              Photoshop, Figma e Illustrator, que se han convertido en imprescindibles en mi día a día.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "HomeView",
+
+  mounted() {
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+            this.observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.2 }
+    );
+
+    this.observeElements();
+  },
+
+  beforeUnmount() {
+    if (this.observer) this.observer.disconnect();
+  },
+
+  methods: {
+    observeElements() {
+      const elements = this.$el.querySelectorAll(".fade-element");
+
+      elements.forEach((el) => {
+        this.observer.observe(el);
+      });
+    },
+  },
 };
 </script>
 
 <style scoped>
-body{
-  padding-top: 80px;
+.body {
+  padding: 80px;
 }
+
+/*SECCION HEADER*/
 .imgHeader {
   margin-top: -70px;
   width: 100%;
@@ -49,7 +113,8 @@ body{
 }
 
 /* PORTFOLIO TEXT */
-.textos p {
+.textos p,
+.section-title {
   margin: 0;
   font-size: clamp(25px, 2vw, 32px);
   color: var(--accent);
@@ -73,10 +138,61 @@ body{
   font-weight: 900;
 }
 
+
+/*SECCION SOBRE MI*/
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+
+.sobre-mi {
+  width: 50%;
+  margin: 0 auto;
+}
+
+.section-title {
+  color: var(--accent);
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+/* SVG derecha */
+.firma svg {
+  width: 120px;
+}
+
+/* TEXTO EN DOS COLUMNAS */
+.explicacionTextos {
+  display: flex;
+  gap: 80px;
+  margin-top: 10px;
+  font-size: clamp(17px, 2vw, 23px);
+  font-weight: bold;
+}
+
+.izquierda,
+.derecha {
+  flex: 1;
+}
+
+/* NO se monte el botón */
+.izquierda {
+  display: flex;
+  flex-direction: column;
+}
+
+.izquierda p {
+  margin-bottom: 30px;
+}
+
 /*TABLET AJUSTES*/
 @media (max-width: 900px) {
-  
+  .sobre-mi {
+    width: 100%;
+  }
 }
+
 /* MÓVIL AJUSTES */
 @media (max-width: 768px) {
   .imgHeader {
@@ -87,6 +203,39 @@ body{
     right: 100px;
     bottom: 280px;
     align-items: flex-end;
+  }
+
+  .sobre-mi {
+    width: 100%;
+  }
+  .firma {
+    width: 150px;
+  }
+
+  .explicacionTextos {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  .izquierda {
+    order: 1;
+    margin: 0px;
+  }
+
+  .derecha {
+    order: 2;
+    margin: 0px;
+  }
+
+  /* botón pasa al final */
+  .izquierda button {
+    order: 3;
+  }
+
+  /* importante para que funcione el order */
+  .izquierda {
+    display: contents;
   }
 }
 </style>
